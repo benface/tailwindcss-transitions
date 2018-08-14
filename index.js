5293a2b96a313d9b76c2771e0f3261f58f2c0a6d
Benoît Rouleau <benoit.rouleau@icloud.com>
2018-08-14 01:17:48 -0400

    fix escaping

--- index.js
+++ index.js
@@ -7,57 +7,54 @@ module.exports = ({
   timingFunctions = {},
   delays = {},
   willChange = {},
-} = {}) =>
-  ({ e, addUtilities }) => {
-    const defaultDuration = durations.default || '500ms';
-    addUtilities(
-      {
-        '.transition-none': { transition: 'none' },
-        ...Object.assign(
-          {},
-          ..._.map(properties, (values, name) => ({
-            [`.transition-${e(name)}`]: {
-              transition: (() => {
-                if (!_.isArray(values)) {
-                  values = [values];
-                }
-                return values.map(value => 
-                  `${value} ${defaultDuration}` + 
-                  (timingFunctions.default ? ' ' + timingFunctions.default : '') +
-                  (delays.default ? ' ' + delays.default : '')
-                ).join(', ');
-              })(),
-            }
-          })),
-          ..._.map(durations, (value, name) => {
-            if (name === 'default') {
-              return null;
-            }
-            return {
-              [`.transition-duration-${e(name)}`]: { transitionDuration: value },
-            };
-          }),
-          ..._.map(timingFunctions, (value, name) => {
-            if (name === 'default') {
-              return null;
-            }
-            return {
-              [`.transition-timing-${e(name)}`]: { transitionTimingFunction: value },
-            };
-          }),
-          ..._.map(delays, (value, name) => {
-            if (name === 'default') {
-              return null;
-            }
-            return {
-              [`.transition-delay-${e(name)}`]: { transitionDelay: value },
-            };
-          }),
-          ..._.map(willChange, (value, name) => ({
-            [`.will-change-${e(name)}`]: { willChange: value },
-          })),
-        ),
-      },
-      variants,
-    );
-  };
+} = {}) => ({ e, addUtilities }) => {
+  const defaultDuration = durations.default || '500ms';
+  addUtilities(
+    {
+      '.transition-none': { transition: 'none' },
+      ...Object.assign(
+        {},
+        ..._.map(properties, (values, name) => ({
+          [`.${e(`transition-${name}`)}`]: {
+            transition: (() => {
+              if (!_.isArray(values)) {
+                values = [values];
+              }
+              return values.map(value => `${value} ${defaultDuration}`
+                  + `${timingFunctions.default ? ` ${timingFunctions.default}` : ''}`
+                  + `${delays.default ? ` ${delays.default}` : ''}`).join(', ');
+            })(),
+          },
+        })),
+        ..._.map(durations, (value, name) => {
+          if (name === 'default') {
+            return null;
+          }
+          return {
+            [`.${e(`transition-duration-${name}`)}`]: { transitionDuration: value },
+          };
+        }),
+        ..._.map(timingFunctions, (value, name) => {
+          if (name === 'default') {
+            return null;
+          }
+          return {
+            [`.${e(`transition-timing-${name}`)}`]: { transitionTimingFunction: value },
+          };
+        }),
+        ..._.map(delays, (value, name) => {
+          if (name === 'default') {
+            return null;
+          }
+          return {
+            [`.transition-delay-${e(name)}`]: { transitionDelay: value },
+          };
+        }),
+        ..._.map(willChange, (value, name) => ({
+          [`.${e(`will-change-${name}`)}`]: { willChange: value },
+        })),
+      ),
+    },
+    variants,
+  );
+};
